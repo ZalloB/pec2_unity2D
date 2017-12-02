e01c92d8d20c589bca098a8a1ce09cff85f8e7df
Beatriz <beatriz.zallo@gmail.com>
2017-12-02 15:10:31 +0100

    start screen, level1 and assets

---
+++ Assets/Scripts/SceneLoadManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SceneLoadManager : MonoBehaviour {
+
+    public void LoadScreen(string name) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
