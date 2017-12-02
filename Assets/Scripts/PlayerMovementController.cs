4b5955dd9136d28334eb6f4ea202474131d2c5ca
beatriz zallo <beatriz.zallo@gmail.com>
2017-12-02 19:40:33 +0100

    new character movement

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	public bool facingRight = true;
+	private Rigidbody2D rigidbody2D;
+	private Animator anim;
+
+	void Start()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
+	}
+
+	void FixedUpdate(){
+		float move = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
