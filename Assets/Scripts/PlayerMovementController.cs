d9bc43fdc6ce232502d56513d3066a65c6845bd8
Beatriz <beatriz.zallo@gmail.com>
2017-12-03 13:26:16 +0100

    jumping, animations and collision2d

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour {
 
 	public float maxSpeed = 10f;
-	public bool facingRight = true;
-	private Rigidbody2D rigidbody2D;
+    public float jumpDistance = 350f;
+    private bool facingRight = true;
+    private bool isGrounded = true;
+
+	private new Rigidbody2D rigidbody2D;
 	private Animator anim;
 
 	void Start()
@@ -17,17 +20,33 @@ public class PlayerMovementController : MonoBehaviour {
 
 	void FixedUpdate(){
 		float move = Input.GetAxis ("Horizontal");
-		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move != 0)
+            anim.SetBool("isWalking", true);
+        else
+            anim.SetBool("isWalking", false);
+
+        rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
 
 		if (move > 0 && !facingRight)
 			Flip ();
 		else if (move < 0 && facingRight)
 			Flip ();
-	}
+
+        if(isGrounded)
+            anim.SetBool("isJumping", false);
+        else
+            anim.SetBool("isJumping", true);
+
+    }
 
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpDistance);
+            isGrounded = false;
+        }
 	}
 
 	void Flip(){
@@ -36,4 +55,12 @@ public class PlayerMovementController : MonoBehaviour {
 		scale.x *= -1;
 		transform.localScale = scale;
 	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ("floor".Equals(collision.gameObject.tag) || "item".Equals(collision.gameObject.tag))
+        {
+            isGrounded = true;
+        }
+    }
 }
