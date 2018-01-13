d0b7844dae74843f2f637fdd618d1bc5d47a6335
Beatriz <beatriz.zallo@gmail.com>
2018-01-13 19:16:28 +0100

    shooting fireball with animations

---
+++ Assets/Scripts/DestroyController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+    public float destroyTime = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, destroyTime);
+	}
+	
+}
